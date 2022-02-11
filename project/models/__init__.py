e07643b85e2ef85cd41bdde13575e300dbbbe9ee
Alilarian <alilarian@yahoo.com>
2022-02-11 15:46:52 +0330

    clear print in methods.py - line 279

--- project/models/__init__.py
+++ project/models/__init__.py
@@ -2,12 +2,11 @@ from ._encoders import get_encoders
 from ._imputers import get_imputer
 from ._ml_algorithms import get_ml_algo
 from ._oversamplers import get_oversampler
-from ._dim_reduction_algorithms import get_dim_reduction_algo
+
 
 __all__ = [
     "get_ml_algo",
     "get_imputer",
     "get_oversampler",
     "get_encoders",
-    "get_dim_reduction_algo",
 ]
